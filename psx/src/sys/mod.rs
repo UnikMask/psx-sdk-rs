5909608e9d040d1dbc055892a38acca8e1abfc67
UnikMask <alexandre.guerville@hotmail.com>
2026-02-12 23:17:30 +0000

    feat(sys): Implement callback-based critical section wrappers

--- psx/src/sys/mod.rs
+++ psx/src/sys/mod.rs
@@ -2,7 +2,9 @@
 //!
 //! This module contains wrappers for functions provided by the BIOS.
 
-use crate::CriticalSection;
+use crate::{hw::{irq, Register},
+            sys::kernel::{psx_enter_critical_section, psx_exit_critical_section},
+            CriticalSection};
 use core::ffi::CStr;
 
 pub mod fs;
@@ -47,4 +49,71 @@ pub fn get_system_version() -> &'static CStr {
 /// Returns the kernel's date in BCD (e.g. 0x19951204).
 pub fn get_system_date() -> u32 {
     unsafe { kernel::psx_get_system_info(0) }
+}
+
+/// Handle for entering and exiting a critical section (all interrupts
+/// disabled), using direct hardware-register based interrupt masking.
+pub struct FastCriticalSection {
+    saved_mask: u16,
+}
+
+/// Handle for entering and exiting a critical section (all interrupts
+/// disabled), using BIOS commands.
+pub struct BiosCriticalSection {
+    return_from_critical_section: bool,
+}
+
+impl FastCriticalSection {
+    /// Enter a new fast critical section
+    pub fn new() -> Self {
+        let mut irq_mask = irq::Mask::new();
+        let saved_mask = *irq_mask.as_ref();
+        irq_mask.assign(0).store();
+        Self { saved_mask }
+    }
+
+    /// Run code within a fast critical section
+    pub fn with<'a, T>(func: impl FnOnce() -> T + 'a) -> T {
+        let mut irq_mask = irq::Mask::new();
+        let saved_mask = *irq_mask.as_ref();
+        irq_mask.assign(0).store();
+        let res = func();
+        irq_mask.assign(saved_mask).store();
+        res
+    }
+}
+
+impl Drop for FastCriticalSection {
+    fn drop(&mut self) {
+        irq::Mask::skip_load().assign(self.saved_mask).store();
+    }
+}
+
+impl BiosCriticalSection {
+    /// Enter a new BIOS critical section
+    pub fn new() -> Self {
+        Self {
+            return_from_critical_section: unsafe { psx_enter_critical_section() },
+        }
+    }
+
+    /// Run code within a fast critical section
+    pub fn with<'a, T>(func: impl FnOnce() -> T + 'a) -> T {
+        let ret = unsafe { psx_enter_critical_section() };
+        let res = func();
+        if ret {
+            unsafe {
+                psx_exit_critical_section();
+            }
+        }
+        res
+    }
+}
+
+impl Drop for BiosCriticalSection {
+    fn drop(&mut self) {
+        if self.return_from_critical_section {
+            unsafe { psx_exit_critical_section() };
+        }
+    }
 }
